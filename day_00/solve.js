51aa292b1b6f627822233413265e85c3fa91217f
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-12-05 11:14:45 +0100

    day_00.js template

---
+++ day_00/solve.js
@@ -0,0 +1,19 @@
+import { equal } from "node:assert";
+import { readLines } from "../helpers.js";
+
+const DAY = `00`;
+
+const part1 = (input) => {};
+
+const part2 = (input) => {};
+
+const example = await readLines(`./day_${DAY}/example.txt`);
+const input = await readLines(`./day_${DAY}/input.txt`);
+
+equal(await part1(example), +Infinity);
+equal(await part1(input), +Infinity);
+
+equal(await part2(example), +Infinity);
+equal(await part2(input), +Infinity);
+
+console.log(`Day ${DAY} completed ✔️`);
