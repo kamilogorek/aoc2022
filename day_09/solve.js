da837ade1e9ccc043df0c107143dc647d7c83242
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-12-10 19:40:59 +0100

    day_09_02.js

--- day_09/solve.js
+++ day_09/solve.js
@@ -4,6 +4,42 @@ import { readLines } from "../helpers.js";
 
 const DAY = `09`;
 
+const DRAWING_PADDING = 5;
+
+const drawRope = (rope) => {
+  const minX = Math.min(...R.pluck("x", rope));
+  const maxX = Math.max(...R.pluck("x", rope));
+  const minY = Math.min(...R.pluck("y", rope));
+  const maxY = Math.max(...R.pluck("y", rope));
+
+  const width = maxX - minX + DRAWING_PADDING * 2;
+  const height = maxY - minY + DRAWING_PADDING * 2;
+
+  const display = Array.from({ length: height }, () => new Array(width).fill("."));
+
+  for (let i = 0; i < rope.length; i++) {
+    const x = rope[i].x - minX + DRAWING_PADDING;
+    const y = rope[i].y - minY + DRAWING_PADDING;
+    if (display[y][x] !== ".") {
+      continue;
+    }
+
+    if (i === 0) {
+      display[y][x] = "H";
+    } else if (i === rope.length - 1) {
+      display[y][x] = "T";
+    } else {
+      display[y][x] = `${i}`;
+    }
+  }
+
+  for (const row of display) {
+    console.log(row.join(""));
+  }
+
+  console.log("");
+};
+
 const DIRS = {
   R: { x: 1, y: 0 },
   L: { x: -1, y: 0 },
@@ -11,95 +47,50 @@ const DIRS = {
   D: { x: 0, y: 1 },
 };
 
-const SHIFT = 20;
-
-const drawRope = (rope) => {
-  const matrix = [];
-  const max = Math.max(...rope.map((v) => v.x + SHIFT + 1));
-
-  rope.forEach((part) => {
-    matrix[part.y + SHIFT] = matrix[part.y + SHIFT] || [];
-    matrix[part.y + SHIFT][part.x + SHIFT] = "X";
-  });
-
-  matrix.forEach((x) => {
-    for (let i = 0; i < max; i++) {
-      x[i] = x[i] || ".";
-    }
-  });
+const segmentsTouching = (current, next) => {
+  return Math.abs(current.x - next.x) <= 1 && Math.abs(current.y - next.y) <= 1;
 };
 
-const move = (dir, rope) => {
+const getPositionDiff = (current, next) => ({
+  x: R.clamp(-1, 1, current.x - next.x),
+  y: R.clamp(-1, 1, current.y - next.y),
+});
+
+const moveRope = (dir, rope) => {
   rope[0].x += DIRS[dir].x;
   rope[0].y += DIRS[dir].y;
 
-  for (let i = 0; i < rope.length - 1; i++) {
-    if (!endsTouching(rope[i], rope[i + 1])) {
-      moveTail(dir, rope[i], rope[i + 1]);
+  for (const [current, next] of R.aperture(2, rope)) {
+    if (segmentsTouching(current, next)) {
+      return;
     }
+
+    const { x, y } = getPositionDiff(current, next);
+    next.x += x;
+    next.y += y;
   }
 };
 
-const moveTail = (dir, head, tail) => {
-  switch (dir) {
-    case "R": {
-      tail.x = head.x - 1;
-      tail.y = head.y;
-      break;
-    }
-    case "L": {
-      tail.x = head.x + 1;
-      tail.y = head.y;
-      break;
-    }
-    case "U": {
-      tail.x = head.x;
-      tail.y = head.y + 1;
-      break;
-    }
-    case "D": {
-      tail.x = head.x;
-      tail.y = head.y - 1;
-      break;
-    }
-  }
-};
-
-const endsTouching = (a, b) => {
-  return b.x >= a.x - 1 && b.x <= a.x + 1 && b.y >= a.y - 1 && b.y <= a.y + 1;
-};
-
-const part1 = (input) => {
-  const rope = Array.from({ length: 2 }, () => ({ x: 0, y: 0 }));
+const solveForLength = (ropeLength, input) => {
+  const rope = Array.from({ length: ropeLength }, () => ({ x: 0, y: 0 }));
   const visited = new Set();
-  const moves = input.map((v) => v.split(" "));
+  const moves = input.map((v) => {
+    const [dir, distance] = v.split(" ");
+    return [dir, parseInt(distance, 10)];
+  });
 
   for (const [dir, distance] of moves) {
     R.times(() => {
-      move(dir, rope);
-      const tail = R.last(rope);
-      visited.add(`${tail.x},${tail.y}`);
-    }, parseInt(distance, 10));
+      moveRope(dir, rope);
+      visited.add(`${R.last(rope).x},${R.last(rope).y}`);
+    }, distance);
   }
 
   return visited.size;
 };
 
-const part2 = (input) => {
-  const rope = Array.from({ length: 10 }, () => ({ x: 0, y: 0 }));
-  const visited = new Set();
-  const moves = input.map((v) => v.split(" "));
-
-  for (const [dir, distance] of moves) {
-    R.times(() => {
-      move(dir, rope);
-      const tail = R.last(rope);
-      visited.add(`${tail.x},${tail.y}`);
-    }, parseInt(distance, 10));
-  }
-
-  return visited.size;
-};
+const part1 = (input) => solveForLength(2, input);
+const part2 = (input) => solveForLength(10, input);
 
 const example = await readLines(`./day_${DAY}/example.txt`);
 const example2 = await readLines(`./day_${DAY}/example_2.txt`);
@@ -108,8 +99,8 @@ const input = await readLines(`./day_${DAY}/input.txt`);
 equal(await part1(example), 13);
 equal(await part1(input), 6498);
 
-// equal(await part2(example), 1);
-// equal(await part2(example2), 36);
-// equal(await part2(input), +Infinity);
+equal(await part2(example), 1);
+equal(await part2(example2), 36);
+equal(await part2(input), 2531);
 
 console.log(`Day ${DAY} completed ✔️`);
