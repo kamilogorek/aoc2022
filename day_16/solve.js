693469e66a526af2727c05e6947902f707b9154d
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-12-18 09:26:27 +0100

    day_16.js - parsing only

---
+++ day_16/solve.js
@@ -0,0 +1,34 @@
+import { equal } from "node:assert";
+import * as R from "ramda";
+import { readLines } from "../helpers.js";
+
+const DAY = `16`;
+
+const parseInput = (input) => {
+  const valves = new Map();
+  input.forEach((line) => {
+    let [_, src, rate, dst] = line.match(/Valve (..) has flow rate=(\d+); tunnels? leads? to valves? (.+)/);
+    valves.set(src, { dst: dst.split(", "), rate: parseInt(rate, 10) });
+  });
+  return valves;
+};
+
+const part1 = (input) => {
+  const start = "AA";
+  const parsed = parseInput(input);
+
+  console.log(parsed);
+};
+
+const part2 = (input) => {};
+
+const example = await readLines(`./day_${DAY}/example.txt`);
+const input = await readLines(`./day_${DAY}/input.txt`);
+
+equal(await part1(example), +Infinity);
+equal(await part1(input), +Infinity);
+
+equal(await part2(example), +Infinity);
+equal(await part2(input), +Infinity);
+
+console.log(`Day ${DAY} completed ✔️`);
