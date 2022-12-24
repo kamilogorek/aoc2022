d03ad749da89a15fdfbcacf909ac48438d3cabdc
Kamil Ogórek <kamil.ogorek@gmail.com>
2022-12-24 16:43:29 +0100

    day_24.js - placeholder

---
+++ day_24/solve.js
@@ -0,0 +1,20 @@
+import { equal } from "node:assert";
+import * as R from "ramda";
+import { readLines } from "../helpers.js";
+
+const DAY = `24`;
+
+const part1 = (input) => {};
+
+const part2 = (input) => {};
+
+const example = await readLines(`./day_${DAY}/example.txt`);
+const input = await readLines(`./day_${DAY}/input.txt`);
+
+equal(await part1(example), +Infinity);
+equal(await part1(input), +Infinity);
+
+equal(await part2(example), +Infinity);
+equal(await part2(input), +Infinity);
+
+console.log(`Day ${DAY} completed ✔️`);
